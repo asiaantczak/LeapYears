d723004665888b42ebd25808be96da69ded18e96
Asia Antczak <aasiaantczak@gmail.com>
2018-06-04 21:37:29 +0100

    simplify the code

--- spec/yearSpec.js
+++ spec/yearSpec.js
@@ -1,22 +1,20 @@
 describe("Year", function() {
 
-  var year = new Year();
+  var year;
 
-  describe("isLeap", function() {
-    it("returns true if a year is divisible by 400", function() {
-      expect(year.isLeap(2000)).toBe(true);
-    });
+  it("returns true if year is divisible by 400", function() {
+    year = new Year(400);
+    expect(year.isLeap()).toBe(true);
+  })
 
-    it("returns false if a year is divisible by 100, but not 400", function() {
-      expect(year.isLeap(1700)).toBe(false);
-    });
+  it("returns true if year is divisible by 4 but not by 100", function() {
+    year = new Year(1996)
+    expect(year.isLeap()).toBe(true)
+  })
 
-    it("returns true if a year is divisible by 4, but not 100", function() {
-      expect(year.isLeap(2008)).toBe(true);
-    });
+  it("returns false if year is divisible by 100 but not 400", function() {
+    year = new Year(1700)
+    expect(year.isLeap()).toBe(false)
+  })
 
-    it("returns false if year is not divisible by 4", function() {
-      expect(year.isLeap(2017)).toBe(false);
-    })
-  });
-});
+})
--- src/year.js
+++ src/year.js
@@ -1,14 +1,11 @@
-function Year() {
-
+function Year(year) {
+  this.year = year;
 }
 
-Year.prototype.isLeap = function(year) {
-  if (year % 400 === 0) {
-    return true;
-  } else if (year % 100 === 0) {
-    return false;
-  } else if (year % 4 === 0) {
-    return true;
-  }
-  return false
+Year.prototype.isLeap = function() {
+  return this._isDivisible(400) || (this._isDivisible(4) && !this._isDivisible(100));
+}
+
+Year.prototype._isDivisible = function(num) {
+  return this.year % num === 0;
 }
